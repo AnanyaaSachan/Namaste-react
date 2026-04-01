99386fd22f6049904ef967ce9556f2e6d9dd68db
Ananya Sachan <shubhiananya1@gmail.com>
2026-04-01 11:39:23 +0530

    jsx

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
- /*
- <div id="parent">
-      <div id="child">
-           <h1>I am h1</h1>
-           <h2>I am h2</h2>
-       </div>
-       <div id="child2">
-            <h1>I am h1</h1>
-           <h2>I am h2</h2>
-       </div>
- </div>
- 
- */
+
  import React from "react";
  import ReactDOM from "react-dom/client";
 
- const parent = React.createElement("div", {id: "parent"},[
-    React.createElement("div", {id: "child", key: "child1" },
-       [React.createElement("h1",{ key: "child1-h1"},"I am h1 tag"),
-        React.createElement("h2",{ key: "child1-h2"},"I am h2 tag")
-       ] 
-    ),
-    React.createElement("div", {id: "child2",key: "child2" },
-       [React.createElement("h1",{ key: "child2-h1"},"I am h1 tag"),
-        React.createElement("h2",{ key: "child2-h2"},"I am h2 tag")
-       ] 
-    )
- ])
+ //using jsx 
+ 
+ const jsxhaeading = <h1 id="heading">Namaste with jsx </h1>;
  
  
  const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+ root.render(jsxhaeading);
